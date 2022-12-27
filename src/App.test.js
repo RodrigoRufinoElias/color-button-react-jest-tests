e9dfedb979e82e96ab8963b723e0ddcd766a533b
Rodrigo Elias <rodrigo.elias@ext.checkr.com>
2022-12-27 16:48:06 -0300

    Add 'replaceCamelWithSpaces' function and its tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App, { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color and updates when clicked', () => {
   render(<App />);
@@ -64,4 +64,18 @@ test('Clicked disable button has gray background and reverts to blue', () => {
   fireEvent.click(colorButton);
   fireEvent.click(checkbox);
   expect(colorButton).toHaveStyle('background-color: gray');
+});
+
+describe('apaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  });
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
 });
